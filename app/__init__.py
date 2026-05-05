fd970467d03840eb7bcf4c2ac825076759b241b7
Ori <orioh15@gmail.com>
2026-05-05 15:43:24 -0400

    Added flask app and database connection. created tables todb

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from app.config import Config
+from app.models import db
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = Config.get_db_url()
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+        print("Database tables created.")
+
+    return app
